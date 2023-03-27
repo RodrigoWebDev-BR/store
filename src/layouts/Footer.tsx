5a9f23f9299ab3bdeedd7445722979ac9296f0f4
Rodrigo Cruz <cruz213.rodrigo@gmail.com>
2023-03-27 09:43:07 -0300

    Update at 27.03.2023 at 09:38

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,161 @@
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <footer className="col-12 container-fluid">
+            <div className="row">
+                <div className="col-12 mb-4 mt-4">
+                    <div className="row">
+                        <div className="col-12 text-center d-flex justify-content-around align-items-center footer">
+                            <div className="col-3 mb-4 mt-4 ms-4">
+                                <form action="" className="d-flex flex-column justify-content-around align-items-center">
+                                    <input type="email" name="" id="email_login" className="mb-2 ms-4 me-4" placeholder="LOGIN" />
+                                    <input type="password" name="" id="senha_login" className="mt-1 mb-2 ms-4 me-4"
+                                        placeholder="Senha" />
+                                    <h4>Não possui cadastro?</h4>
+                                    <input type="email" name="" id="cad_email" className="mt-1 mb-1 ms-4 me-4"
+                                        placeholder="E-Mail" />
+                                    <input type="email" name="" id="conf_email" className="mt-2 mb-1 ms-4 me-4"
+                                        placeholder="Confirmar E-Mail" />
+                                    <input type="password" name="" id="senha_cad" className="mt-2 mb-1 ms-4 me-4"
+                                        placeholder="Senha" />
+                                    <input type="password" name="" id="conf_senha" className="mt-2 mb-1 ms-4 me-4"
+                                        placeholder="Confirmar senha" />
+                                </form>
+                            </div>
+
+                            <div className="col-6 ms-4 d-flex justify-content-around align-items-center">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <div className="row">
+                                            <div className="col-3">
+                                                <ul>
+                                                    <li>FEMININO</li>
+                                                    <li>Termos de Uso</li>
+                                                    <li>Política de Privacidade</li>
+                                                    <li>Trabalhe Conosco</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-3">
+                                                <ul>
+                                                    <li>ISNTITUCIONAL</li>
+                                                    <li>Termos de Uso</li>
+                                                    <li>Política de Privacidade</li>
+                                                    <li>Trabalhe Conosco</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-3">
+                                                <ul>
+                                                    <li>ISNTITUCIONAL</li>
+                                                    <li>Termos de Uso</li>
+                                                    <li>Política de Privacidade</li>
+                                                    <li>Trabalhe Conosco</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-3">
+                                                <ul>
+                                                    <li>ISNTITUCIONAL</li>
+                                                    <li>Termos de Uso</li>
+                                                    <li>Política de Privacidade</li>
+                                                    <li>Trabalhe Conosco</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-3 mb-4 mt-4">
+                                <div className="row">
+                                    <div className="col-12 payments">
+                                        <div className="row">
+                                            <div className="col-12 mb-1 mt-1 d-flex justify-content-evenly align-items-center">
+                                                <img src="./assets/img/icons/payment/elo_pament_method_icon_142756.svg"
+                                                    alt="elo" width="40px" height="40px" />
+                                                <img src="./assets/img/icons/payment/-mastercard_86833.svg"
+                                                    alt="master_card" width="40px" height="40px" />
+                                                <img src="./assets/img/icons/payment/if-visa-electron-2593672_86617.svg"
+                                                    alt="visa" width="40px" height="40px" />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12 mb-1 mt-1 d-flex justify-content-evenly align-items-center">
+                                                <img src="./assets/img/icons/payment/hipercard_payment_method_card_icon_142739.svg"
+                                                    alt="hipercard" width="40px" height="40px" />
+                                                <img src="./assets/img/icons/payment/amex_payment_method_card_icon_142744.svg"
+                                                    alt="amex" width="40px" height="40px" />
+                                                <img src="./assets/img/icons/payment/diners_payment_method_card_icon_142742.svg"
+                                                    alt="dinners" width="40px" height="40px" />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12 mb-1 mt-1 d-flex justify-content-evenly align-items-center">
+                                                <img src="./assets/img/icons/payment/paypal_payment_method_card_icorown_142733.svg"
+                                                    alt="playpal" width="40px" height="40px" />
+                                                <img src="./assets/img/icons/payment/pix-106.svg" alt="pix" width="40px"
+                                                    height="40px" />
+                                                <img src="./assets/img/icons/payment/boleto-logo.svg" alt="boleto"
+                                                    width="40px" height="40px" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12">
+                    <div className="row">
+                        <div className="col-6 d-flex justify-content-center align-items-center social-media">
+                            <div className="row">
+                                <div className="col-12">
+                                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/facebook_icon-icons.com_53612.svg"
+                                            alt="facebook" width="40px" height="40px" className="ms-2 me-2" /> </a>
+                                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/3721672-instagram_108066.svg"
+                                            alt="instagram" width="40px" height="40px" className="ms-2 me-2" /> </a>
+                                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/in_linked_linkedin_media_social_icon_124259.svg"
+                                            alt="linkedin" width="40px" height="40px" className="ms-2 me-2" />
+                                    </a>
+                                    <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/3721679-youtube_108064.svg" alt="youtube"
+                                            width="40px" height="40px" className="ms-2 me-2" />
+                                    </a>
+                                    <a href="https://twitter.com/i/flow/login" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/1492693505-1-twitter_83571.svg"
+                                            alt="twitter" width="40px" height="40px" className="ms-2 me-2" />
+                                    </a>
+                                    <a href="https://br.pinterest.com/login" target="_blank" rel="noopener noreferrer">
+                                        <img src="./assets/img/icons/social_media/pinterest_icon-icons.com_65931.svg"
+                                            alt="pinterest" width="40px" height="40px" className="ms-2 me-2" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-6 d-flex align-items-end">
+                            <div className="row">
+                                <div className="col-12">
+                                    <p className="text-center">&copy; TÊNNIS STORE - 2023 - Todos os direitos reservados</p>
+                                    <p className="vetado">É vetada a reprodução total ou parcial das informações
+                                        aqui veiculadas sem
+                                        a expressa autorização da administração do site. Os preços e condições de
+                                        pagamento são válidos exclusivamente para compras realizadas via internet.
+                                        Os estoques são limitados e os valores não se aplicam à nossa rede de lojas
+                                        físicas podendo sofrer alterações sem aviso prévio.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 mt-4 text-center container-fluid">
+                            <p>Website criado e desenvolvido por <a href="https://rodrigowebdev.netlify.app" target="_blank"
+                                rel="noopener noreferrer">Rodrigo Cruz</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
